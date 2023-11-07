5ef401a8c376f459d9c3e6c204235cec7368340f
Judah Daniels <judahdaniels@outlook.com>
2023-11-07 14:02:10 +0000

    Highlights light and dark

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -14,4 +14,10 @@ function SetupColor(color)
   vim.api.nvim_set_hl(0, "jukit_cellmarker_colors", { bg = "#1d213a" })
 end
 
-SetupColor()
+local iterm_profile = os.getenv("ITERM_PROFILE")
+
+if iterm_profile == "light" then
+  SetupColor("rose-pine-dawn")
+else
+  SetupColor("rose-pine")
+end
